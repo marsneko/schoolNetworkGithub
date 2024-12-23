131af0fc446c56477c213f7f24e3212dcb61c642
marsneko <79626049+marsneko@users.noreply.github.com>
2024-12-23 13:03:33 +0800

    add try except part in getstudent function in  crossbystudentwithRank.py

--- crawler/crossbystudentwithRank.py
+++ crawler/crossbystudentwithRank.py
@@ -10,32 +10,36 @@ year = 102
 def getstudents(soup, url):
     connlist = []
     url = url.replace('https://www.com.tw/cross/', "")
-    for rank,col in enumerate(soup.find_all('table', width='99%')[0].tbody.contents):
-        if col == '\n' or rank < 6:
-            continue
-        try:
-            temp = {}
-            temp['rank'] = (rank - 5) // 2 + 1
-            temp['self_url'] = url
-            temp['self_tag'] = col.contents[3].contents[0].img['src']
-            temp['edges'] = {} #{edge:[chinese name,accept,tag]}
-            for sector in col.find(colspan = "4").contents[0].find('table').tbody.contents:
-                if sector.text.replace('\n', '') == '':
-                    continue
-                temp['edges'][sector.contents[3].a['href']] = {
-                    'chinese_name': sector.contents[3].a.text,
-                    'accept': True if sector.contents[1].img != None else False,
-                    'tag': sector.contents[5].img['src'] if sector.contents[5].img != None else None
-                }
-        except:
-            log = open(f"./crossbystd_{year}/log.txt", "a")
-            log.write(f"Error: {url}\n")
-            log.close()
-            continue
-        print(temp,end='\n')
-        connlist.append(temp)
-
-
+    try:
+        for rank,col in enumerate(soup.find_all('table', width='99%')[0].tbody.contents):
+            if col == '\n' or rank < 6:
+                continue
+            try:
+                temp = {}
+                temp['rank'] = (rank - 5) // 2 + 1
+                temp['self_url'] = url
+                temp['self_tag'] = col.contents[3].contents[0].img['src']
+                temp['edges'] = {} #{edge:[chinese name,accept,tag]}
+                for sector in col.find(colspan = "4").contents[0].find('table').tbody.contents:
+                    if sector.text.replace('\n', '') == '':
+                        continue
+                    temp['edges'][sector.contents[3].a['href']] = {
+                        'chinese_name': sector.contents[3].a.text,
+                        'accept': True if sector.contents[1].img != None else False,
+                        'tag': sector.contents[5].img['src'] if sector.contents[5].img != None else None
+                    }
+            except:
+                log = open(f"./crossbystd_{year}/log.txt", "a")
+                log.write(f"Error: {url}\n")
+                log.close()
+                continue
+            print(temp,end='\n')
+            connlist.append(temp)
+    except:
+        log = open(f"./crossbystd_{year}/log.txt", "a")
+        log.write(f"Error: {url}\n")
+        log.close()
+        connlist.append({'rank': None, 'self_url': url, 'self_tag': None, 'edges': {}})
 
     return url, connlist
 
